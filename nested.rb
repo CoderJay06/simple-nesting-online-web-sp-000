bb615e84cfe1a6d69a54ce6e4e581ecaf7a8413b
Jason J Cruz <jaycru06@yahoo.com>
2019-11-25 21:53:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,8 +102,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = programmer_hash[:alan_kay][:known_for].replace(:known_for => "GUI")
-     
+     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for][0] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
